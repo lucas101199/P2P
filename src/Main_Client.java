05057766a132c399612c3b1f363dc2439bcc0d4a
Lucas Fernandez <ens19lfz@cs.umu.se>
2019-10-29 19:24:10 +0100

    add class Main for testing purpose only

---
+++ src/Main_Client.java
@@ -0,0 +1,17 @@
+import Torrent.Torrent;
+import java.io.File;
+import java.net.URL;
+
+public class Main_Client {
+
+    public static void main(String args[]) throws Exception {
+        File file = new File("/home/ens19/ens19lfz/edu/5DV205/assignments/project/src/Torrent/test_image.png");
+        Torrent torrent = new Torrent(new URL("http://localhost:8080"), file, 256);
+        byte[] by = torrent.gethash();
+        System.out.println(by);
+        Client client = new Client(20, 9000);
+
+        client.request(client.peer_id, client.port, client.getUrl(torrent).getPort(), torrent);
+
+    }
+}
---
+++ src/Torrent/main.java
@@ -0,0 +1,15 @@
+package Torrent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+
+public class main {
+
+    public static void main(String args[]) throws IOException, NoSuchAlgorithmException {
+        File fi = new File("/home/ens19/ens19lfz/edu/5DV205/assignments/project/src/Torrent/test_image.png");
+
+        //Torrent to = new Torrent(new URL("http://www.example.com/docs/resource1.html"), fi, 256);
+    }
+}
